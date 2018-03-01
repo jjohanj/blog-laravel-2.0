91740c1dcad83bd41e64302a45fa5d487500de1a
jjohanj <johanjorritsma@hotmail.com>
2018-03-01 15:55:06 +0100

    bug fixed

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -11,14 +11,14 @@
 <button onclick="window.location.href = '/admin-articles';">Admin articles</button><br><hr>
 
 <h3>Create a Post!</h3>
+
+
 <div id="form">
   <form action="/posts/create/category" method="POST">
     {{csrf_field()}}
-    <button type="submit">Add category:</button>
-    <input placeholder="Title" name="addcategory" type="text" id="addcategory"><br><hr>
+    <button type="submit">Add a new category:</button>
+    <input placeholder="new category name" name="addcategory" type="text" id="addcategory"><br><hr>
 
-  </form>
-</div>
 
 <div id="form">
   <form action="/posts" method="POST">
@@ -26,7 +26,10 @@
     <b>Title:</b> <input placeholder="Title" name="title" type="text" id="title"><hr>
     <b>Category:</b> @foreach ($categories as $category)
         <input type="radio" name="category" value="{{ $category->category }}" checked> {{ $category->category }}
-      @endforeach<hr>
+      @endforeach
+      <div id="form">
+
+      </div><br><br>
     <input type="hidden" name="comments" value="1">
     <textarea rows="5" placeholder="Blog text" name="body" type="text" id="body"></textarea><br>
     <button type="submit">Submit Blog!</button><br>
